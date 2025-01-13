191eeed48c78018db29fa219d0b23e951085e3f6
Kaylon-Riordan <kaylonriordan@gmail.com>
2025-01-13 18:04:40 +0000

    ✨ Added win conditions and resistor types
    added win conditions for last 2 circuits, also added all needed resistor values.

---
+++ Assets/My Assets/Scripts/States/CircuitCondition3.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using GD.Items;
+using NUnit.Framework;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GD.State
+{
+    /// <summary>
+    /// A condition that circuit 3 is correctly completed.
+    /// </summary>
+    [CreateAssetMenu(fileName = "CircuitCondition3", menuName = "GD/Conditions/Single/C3", order = 2)]
+    public class CircuitCondition3 : ConditionBase
+    {
+        [FoldoutGroup("Slots", expanded: true)]
+        [SerializeField]
+        private Transform slot1;
+        [FoldoutGroup("Slots")]
+        [SerializeField]
+        private Transform slot2;
+        [FoldoutGroup("Slots")]
+        [SerializeField]
+        private Transform slot3;
+        [FoldoutGroup("Slots")]
+        [SerializeField]
+        private Transform slot4;
+        [FoldoutGroup("Slots")]
+        [SerializeField]
+        private Transform slot5;
+        [FoldoutGroup("Slots")]
+        [SerializeField]
+        private Transform slot6;
+
+        [FoldoutGroup("Data", expanded: true)]
+        [SerializeField]
+        private ItemData data1;
+        [FoldoutGroup("Data")]
+        [SerializeField]
+        private ItemData data2;
+        [FoldoutGroup("Data")]
+        [SerializeField]
+        private ItemData data3;
+        [FoldoutGroup("Data")]
+        [SerializeField]
+        private ItemData data4;
+
+        [FoldoutGroup("Lights", expanded: true)]
+        [SerializeField]
+        private Light light1;
+        [FoldoutGroup("Lights")]
+        [SerializeField]
+        private Light light2;
+        [FoldoutGroup("Lights")]
+        [SerializeField]
+        private Light light3;
+        [FoldoutGroup("Lights")]
+        [SerializeField]
+        private Light light4;
+        [FoldoutGroup("Lights")]
+        [SerializeField]
+        private Light light5;
+        [FoldoutGroup("Lights")]
+        [SerializeField]
+        private Light light6;
+
+        protected override bool EvaluateCondition(ConditionContext conditionContext)
+        {
+            if (evaluate(slot1, slot2, slot3, slot4, slot5, slot6, light5, light6))
+            {
+                return true;
+            }
+            else
+            {
+                light1.intensity = 0f;
+                light2.intensity = 0f;
+                light3.intensity = 0f;
+                light4.intensity = 0f;
+                light5.intensity = 0f;
+                light6.intensity = 0f;
+                return false;
+            }
+        }
+
+        private bool evaluate(Transform s1, Transform s2, Transform s3, Transform s4, Transform s5, Transform s6, Light l1, Light l2)
+        {
+            if (s1.GetComponent<Slot>().slotedItem == data1 && s2.GetComponent<Slot>().slotedItem == data1 && ((s3.GetComponent<Slot>().slotedItem == data2 && s4.GetComponent<Slot>().slotedItem == data3) 
+                || (s3.GetComponent<Slot>().slotedItem == data3 && s4.GetComponent<Slot>().slotedItem == data2)) && s5.GetComponent<Slot>().slotedItem == data4 && s6.GetComponent<Slot>().slotedItem == data4)
+            {
+                l1.intensity = 500f;
+                l2.intensity = 500f;
+                return true;
+            }
+            return false;
+        }
+    }
+}
